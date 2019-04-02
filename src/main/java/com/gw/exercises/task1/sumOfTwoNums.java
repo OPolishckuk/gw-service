5cec8141f3b41ae6cae081e64799a09e847f273e
Konstantine Kirichenko <bkoss@ukr.net>
2019-04-03 00:56:37 +0300

    Перші таски
    І'мя
    Множення
    Ділення
    Додавання
    Формули

---
+++ src/main/java/com/gw/exercises/task1/sumOfTwoNums.java
@@ -0,0 +1,16 @@
+package com.gw.exercises.task1;
+
+public class sumOfTwoNums {
+
+    public static void main(String[] args) {
+        //        Напиши програму яка обраховує суму двох чисел (наприклад 13 та 26).
+
+        System.out.println(sum(13, 26));
+
+    }
+    private static int sum(int a, int b) {
+
+        return a + b;
+
+    }
+}
